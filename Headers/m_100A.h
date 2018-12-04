78f17285ab4a5e72a3aecaa9d756aae4a4d2278a
alitobit <32698482+alitobit@users.noreply.github.com>
2018-12-04 06:15:13 -0800

    Add files via upload

---
+++ Headers/m_100A.h
@@ -0,0 +1,29 @@
+#ifndef m_100A_H
+#define m_100A_H
+#include<string>
+#include<iomanip>
+
+using namespace std;
+
+class m_100A: public Machine
+{
+
+private:
+
+	string 
+
+
+public:
+
+	m_100A();
+	
+
+
+	void openfileItems()
+
+	
+
+
+
+}
+#endif
