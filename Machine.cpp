ce1d79e72ee9a9c7034c58d4102b70abb4e832d6
alitobit <32698482+alitobit@users.noreply.github.com>
2018-12-04 02:26:14 -0800

    Update Machine.cpp

--- Machine.cpp
+++ Machine.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 #include"Machine.h"
 
 using namespace std;
@@ -9,13 +10,27 @@ void Machine::Machine::machine()
 
 }
 
-void Machine::Machine::selected(string& machine)
+//Machine selection
+void Machine::Machine::selected(string& machine, fstream& file, fstream& list)
 {
-	
-	
+	while (!list.eof()) {
+
+	}
+
 	string select;
 	select = machine;
-	if (select == "100A")
+	
+	if (select == "100A1")
+	{
 		
+		cout << "This machine accepts one-dollar bills only.\n";
+
+	}
+	else
+	{
+
+	}
+
+
 
 }
