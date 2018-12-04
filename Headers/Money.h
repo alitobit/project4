4d7e88f593871395c6613ca4f30de252742ed5f0
alitobit <32698482+alitobit@users.noreply.github.com>
2018-12-04 04:11:14 -0800

    Update Money.h

--- Headers/Money.h
+++ Headers/Money.h
@@ -1,18 +1,48 @@
+
 #ifndef Money_H
 #define Money_H
+
 class Money
 {
-
 private:
-
-	int Dollar = 100;
-	int Quarter = 25;
-	int Dime = 10;
-	int Nickle = 5;
-
+	int value;
+	int amount;
+//value of currency
+	int dollars = 100,
+	int quarters = 25,
+	int dimes = 10, 
+	int nickles = 5;
 public:
 
+	Money();
+
+	Money(int newValue, int newAmount,int,int);
+
+	void increase(int d,int q,int di,int n);
+
+	void decrease(int d, int q, int di, int n);
+//setters
+	void setDollars(int d);
+
+	void setQuarters(int q);
+
+	void setDimes(int di);
+
+	void setNickels(int n);
+//getters
+	int getValue();
+
+	int getAmount();
+	
+	int getDollars();
+
+	int getQuarters();
+
+	int getDimes();
+
+	int getNickels();
 
 
 };
-#endif
+
+#endif 
